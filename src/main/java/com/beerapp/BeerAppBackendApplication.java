232704a3e55d1636729d5496d04832547c22d6b0
Miguel <miguelgomezaguirre@hotmail.com>
2020-04-20 10:14:44 -0300

    first commit

---
+++ src/main/java/com/beerapp/BeerAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerAppBackendApplication.class, args);
+	}
+
+}
